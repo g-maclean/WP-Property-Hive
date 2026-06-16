50b1bc2f09ae5f3e0da786d6428bd6a9abdc659d
g-maclean <maclean.grantuk@gmail.com>
2026-06-16 02:54:27 +0100

    fix elementor call propertyhive_get_flag function

--- includes/class-ph-elementor.php
+++ includes/class-ph-elementor.php
@@ -432,7 +432,7 @@ class PH_Elementor {
 
         if ( isset($settings['show_flag']) && $settings['show_flag'] == 'yes' )
         {
-            $flag = $this->propertyhive_get_flag();
+            $flag = propertyhive_get_flag();
 
             if ( $flag != '' )
             {
