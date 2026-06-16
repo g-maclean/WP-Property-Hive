1f1e02d5344ce256b0b1f6c81a7aca60d479801a
g-maclean <maclean.grantuk@gmail.com>
2026-06-16 02:56:53 +0100

    Revert "Replace non existent get_flag with propertyhive_get_flag method in el…"
    This reverts commit fdb9c764bbaada02608a4aedc3602e3a650ab4a1.

--- includes/class-ph-elementor.php
+++ includes/class-ph-elementor.php
@@ -432,7 +432,7 @@ class PH_Elementor {
 
         if ( isset($settings['show_flag']) && $settings['show_flag'] == 'yes' )
         {
-            $flag = propertyhive_get_flag();
+            $flag = $this->get_flag();
 
             if ( $flag != '' )
             {
